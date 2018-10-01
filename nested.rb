5d7d1457bf354f67a74f45921cf238843d1acfc8
Ana Harris <vana1990@gmail.com>
2018-10-01 03:33:04 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -55,7 +55,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-     return programmer_hash[:dennis_ritchie][:languages]
+    programmer_hash[:dennis_ritchie][:languages]
+    return 
 end
 
 def adding_matz
